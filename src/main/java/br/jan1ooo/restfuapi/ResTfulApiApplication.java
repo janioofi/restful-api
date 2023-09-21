3d57731575d8c07737fd37fdcc5e3dc9f860c166
Janio Filho <filhojanio67@gmail.com>
2023-09-20 21:52:07 -0300

    First commit

---
+++ src/main/java/br/jan1ooo/restfuapi/ResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package br.jan1ooo.restfuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/jan1ooo/restfuapi/ResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jan1ooo.restfuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
